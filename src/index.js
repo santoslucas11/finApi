a2c1ac3715dde705dc854024da3cee1e1b37825b
Lucas <santoslucas.dev@gmail.com>
2021-09-16 21:14:48 -0300

    criando depósito em conta

--- src/index.js
+++ src/index.js
@@ -51,4 +51,21 @@ app.get("/extrato", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
 app.listen(3333);
